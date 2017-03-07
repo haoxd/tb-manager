c539e99cb2879dde2534906d3fc96513eefea97b
郝旭东 <haoxd@njhhet.com>
2017-03-07 22:03:39 +0800

    内容类目service，dao，pojo

---
+++ tb-manager-dao/src/main/java/com/tb/manager/dao/content/ContentCategoryDao.java
@@ -0,0 +1,19 @@
+package com.tb.manager.dao.content;
+
+import com.github.abel533.mapper.Mapper;
+import com.tb.manager.pojo.ContentCategory;
+
+/**
+ * @author acer11
+ *  作者：haoxud	
+* 创建时间：2017年3月7日 下午9:58:54  
+* 项目名称：tb-manager-dao  
+* @author daniel  
+* @version 1.0   
+* @since JDK 1.6.0_21  
+* 文件名称：ContentCategoryDao.java  
+* 类说明：内容类目dao
+ */
+public interface ContentCategoryDao extends Mapper<ContentCategory>{
+
+}
---
+++ tb-manager-service/src/main/java/com/tb/manager/service/content/ContentCategoryService.java
@@ -0,0 +1,22 @@
+package com.tb.manager.service.content;
+
+import org.springframework.stereotype.Service;
+
+import com.tb.manager.pojo.ContentCategory;
+import com.tb.manager.service.base.BaseService;
+
+/**
+ * @author acer11
+ *  作者：郝旭东
+* 创建时间：2017年3月7日 下午10:02:23  
+* 项目名称：tb-manager-service  
+* @author daniel  
+* @version 1.0   
+* @since JDK 1.6.0_21  
+* 文件名称：ContentCategoryService.java  
+* 类说明：内容类目服务
+ */
+@Service("contentCategoryService")
+public class ContentCategoryService extends BaseService<ContentCategory>{
+
+}
